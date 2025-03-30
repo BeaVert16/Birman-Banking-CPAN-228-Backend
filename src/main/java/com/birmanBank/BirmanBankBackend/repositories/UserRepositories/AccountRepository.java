2f2cf9ea5b383f4ec999c3e061eff8a50be1b414
Alex P <afapenha@gmail.com>
2025-03-30 11:33:05 -0400

    fixed and made respective repo's for models

---
+++ src/main/java/com/birmanBank/BirmanBankBackend/repositories/UserRepositories/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.birmanBank.BirmanBankBackend.repositories.UserRepositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.birmanBank.BirmanBankBackend.models.Account;
+
+import java.util.List;
+
+public interface AccountRepository extends MongoRepository<Account, String> {
+    //retrieve all accounts for a specific client (one-to-many relationship)
+    //will return a list of accounts associated with the given client ID
+    List<Account> findByClientId(String clientId);
+}
+
---
+++ src/main/java/com/birmanBank/BirmanBankBackend/repositories/UserRepositories/TransactionRepository.java
@@ -0,0 +1,13 @@
+package com.birmanBank.BirmanBankBackend.repositories.UserRepositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.birmanBank.BirmanBankBackend.models.Transaction;
+
+import java.util.List;
+
+public interface TransactionRepository extends MongoRepository<Transaction, String> {
+    //retrieve all transactions for a specific account (one-to-many relationship)
+    //will return a list of transactions associated with the given account ID
+    List<Transaction> findByAccountId(String accountId);
+}
